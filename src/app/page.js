d4502bf5f95349fb513964224cfcb60a5cb2baea
Abhiraj-Sagar <abhiraj.sagar@soniconsultancy.in>
2026-03-18 13:35:00 +0530

    Fixed Layouting for Mobile Devices

--- src/app/page.js
+++ src/app/page.js
@@ -37,12 +37,12 @@ export default function Home()
     return <QuizPage onBack={() => setSelectedQuiz(null)} title={selectedQuiz.title} quiz={selectedQuiz.questions}/>
 
   return (
-    <div className="p-12 bg-gray-900 min-h-screen flex flex-col items-center">
-      <div className="w-full max-w-4xl mx-auto rounded-lg bg-gray-800 px-6 py-4 flex justify-between items-center shadow-lg mb-4">
+    <div className="p-2 sm:p-12 bg-gray-900 min-h-screen flex flex-col items-center">
+      <div className="w-full max-w-4xl mx-auto rounded-t-lg bg-gray-800 px-6 py-4 flex justify-between items-center shadow-lg mb-1">
         <h2 className="text-white text-3xl text-center w-full font-bold tracking-wide">Create Quiz</h2>
       </div>
 
-      <div className="w-full max-w-4xl mx-auto p-8 bg-gray-800 rounded-lg shadow-xl flex flex-col gap-6">
+      <div className="w-full max-w-4xl mx-auto p-4 sm:p-4 bg-gray-800 shadow-xl flex flex-col gap-4 sm:gap-6">
         <p className="w-full text-center text-white text-lg">
           Enter your topic and other details.
         </p>
@@ -98,7 +98,7 @@ export default function Home()
         </div>
       </div>
 
-      <div className="w-full max-w-4xl mx-auto p-4 bg-gray-800 mt-4 rounded-lg shadow-xl flex flex-col gap-4">
+      <div className="w-full max-w-4xl mx-auto p-4 bg-gray-800 mt-1 rounded-b-lg shadow-xl flex flex-col gap-4">
         <h2 className="text-lg font-semibold">Saved Quizzes</h2>
         {localQuizzes.map((quiz, index) => (
           <div key={index} onClick={() => setSelectedQuiz(quiz)} className="w-full bg-gray-700 h-14 rounded-md flex px-5 justify-start items-center cursor-pointer hover:translate-y-1">
